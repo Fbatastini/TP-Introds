ce49affe8f9dec138f6504800efc1b6399562331
maximovasil <maxivasil12@gmail.com>
2024-06-03 14:08:02 -0300

    Agrego imagenes, elimino iconos, arreglo 404

--- static/js/main.js
+++ static/js/main.js
@@ -60,26 +60,6 @@
     time: 2000,
   });
 
-  // Modal Video
-  $(document).ready(function () {
-    var $videoSrc;
-    $(".btn-play").click(function () {
-      $videoSrc = $(this).data("src");
-    });
-    console.log($videoSrc);
-
-    $("#videoModal").on("shown.bs.modal", function (e) {
-      $("#video").attr(
-        "src",
-        $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
-      );
-    });
-
-    $("#videoModal").on("hide.bs.modal", function (e) {
-      $("#video").attr("src", $videoSrc);
-    });
-  });
-
   // Testimonials carousel
   $(".testimonial-carousel").owlCarousel({
     autoplay: true,
