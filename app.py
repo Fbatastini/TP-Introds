e22caf17d29c65f5ff14d55ff7e3cd382c3e33a3
JuaniMoore <jmoore@fi.uba.ar>
2024-06-03 16:39:35 -0300

    Modifiqué footer, incluí términos y condiciones, y políticas de privacidad.

--- app.py
+++ app.py
@@ -38,6 +38,14 @@ def team():
 def testimonial():
     return render_template('testimonial.html')
 
+@app.route('/terms')
+def terms():
+    return render_template('terms.html')
+
+@app.route('/privacy')
+def privacy():
+    return render_template('privacy.html')
+
 # Ruta para manejar la carga de archivos
 if __name__ == '__main__':
     app.run(debug=True)
