ca6cca4f9cb53772c033113de61641aa81ea52c9
Fabricio Batastini <fabatastini@fi.uba.ar>
2024-06-04 02:42:58 -0300

    Login casi funcionando #23 Falta ver si funciona verificar al usuario desde la base Falta crear la pagina de admin

--- app.py
+++ app.py
@@ -1,10 +1,23 @@
 # app.py
 from flask import Flask, render_template, request, redirect, url_for
 from datetime import datetime
+# import requests
+from flask_login import LoginManager, login_user, logout_user, login_required, UserMixin
 
 app = Flask(__name__)
 app.secret_key = 'your_secret_key'  # Necesario para usar flash messages
 
+login_manager = LoginManager()
+login_manager.init_app(app)
+login_manager.login_view = 'login'
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User(user_id)
+
+class User(UserMixin):          #Usermixin genera los metodos necesarios para que flask-login funcione, como get_id() o is_active()
+    def __init__(self, id):
+        self.id = id
 
 # Ruta principal para la página de inicio
 @app.route('/')
@@ -61,6 +74,33 @@ def testimonial():
 @app.route('/confirmacion')
 def confirmacion():
     return render_template('confirmacion.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        user = request.form.get('username')
+        passwd = request.form.get('password')
+        # respuesta = requests.get(f'http://127.0.0.1:5001/usuario/{user}/{passwd}').json()
+        # if respuesta[1] == 200:
+        if user == 'juan' and passwd =='123':
+            usuario = User(user)
+            login_user(usuario)
+            return redirect(url_for('admin'))
+        else:
+            return render_template('login.html', mensaje = 'Wrong user or password')
+    logout_user()
+    return render_template('login.html')
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('index'))
+
+@app.route('/admin')
+@login_required
+def admin():
+    return render_template('admin.html')
 
 # Ruta para manejar la carga de archivos
 if __name__ == '__main__':
