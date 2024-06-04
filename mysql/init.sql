5cfcba655d9222133cdd9e56a94405a9f73d7a2b
Fabricio Batastini <fabatastini@fi.uba.ar>
2024-06-04 19:26:08 -0300

    Revert "Cambio en numero_reserva tabla reservas  #4."
    This reverts commit 01cf24f4780e212b142c73dc8ebf2a56dc099fa5.

--- mysql/init.sql
+++ mysql/init.sql
@@ -1,5 +1,5 @@
 CREATE TABLE reservas (
-    numero_reserva INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    numero_reserva INT NOT NULL PRIMARY KEY,
     nombre VARCHAR(60) NOT NULL,
     mail VARCHAR(120) NOT NULL,
     tipo_habitacion VARCHAR(10) NOT NULL,
@@ -15,14 +15,14 @@ CREATE TABLE habitaciones (
 );
 
 /* Agregamos datos a la tabla reservas */
-INSERT INTO reservas (nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES ("Manuel", "manuel@fi.uba.ar", "normal", 4, "2024-05-29", "2024-05-31");
-INSERT INTO reservas (nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES ("Carlos", "carlos@fi.uba.ar", "deluxe", 2, "2024-05-30", "2024-06-05");
-INSERT INTO reservas (nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES ("Juan", "juanP@fi.uba.ar", "normal", 1, "2024-05-31", "2024-06-04");
-INSERT INTO reservas (nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES ("Fabricio", "fabricio@fi.uba.ar", "premium", 3, "2024-06-01", "2024-06-04");
-INSERT INTO reservas (nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES ("Valentin", "valentin@fi.uba.ar", "normal", 5, "2024-06-02", "2024-06-05");
-INSERT INTO reservas (nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES ("Julieta", "julieta@fi.uba.ar", "premium", 3, "2024-06-03", "2024-06-08");
-INSERT INTO reservas (nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES ("Florencia", "florencia@fi.uba.ar", "normal", 4, "2024-06-04", "2024-06-10");
-INSERT INTO reservas (nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES ("Carla", "carla@fi.uba.ar", "deluxe", 2, "2024-06-04", "2024-06-09");
+INSERT INTO reservas (numero_reserva, nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES (1, "Manuel", "manuel@fi.uba.ar", "normal", 4, "2024-05-29", "2024-05-31");
+INSERT INTO reservas (numero_reserva, nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES (2, "Carlos", "carlos@fi.uba.ar", "deluxe", 2, "2024-05-30", "2024-06-05");
+INSERT INTO reservas (numero_reserva, nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES (3, "Juan", "juanP@fi.uba.ar", "normal", 1, "2024-05-31", "2024-06-04");
+INSERT INTO reservas (numero_reserva, nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES (4, "Fabricio", "fabricio@fi.uba.ar", "premium", 3, "2024-06-01", "2024-06-04");
+INSERT INTO reservas (numero_reserva, nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES (5, "Valentin", "valentin@fi.uba.ar", "normal", 5, "2024-06-02", "2024-06-05");
+INSERT INTO reservas (numero_reserva, nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES (6, "Julieta", "julieta@fi.uba.ar", "premium", 3, "2024-06-03", "2024-06-08");
+INSERT INTO reservas (numero_reserva, nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES (7, "Florencia", "florencia@fi.uba.ar", "normal", 4, "2024-06-04", "2024-06-10");
+INSERT INTO reservas (numero_reserva, nombre, mail, tipo_habitacion, cantidad_personas, fecha_ingreso, fecha_salida) VALUES (8, "Carla", "carla@fi.uba.ar", "deluxe", 2, "2024-06-04", "2024-06-09");
 
 /* Agregamos datos a la tabla habitaciones */
 INSERT INTO habitaciones (tipo_habitacion, cantidad_total) VALUES ("normal", 10);
